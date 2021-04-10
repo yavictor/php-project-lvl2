c4736ac4f795843c1af38cdab136c7842d1b9357
yavictor <31189356+yavictor@users.noreply.github.com>
2021-04-10 13:32:04 +0300

    Update DiffGenerator.php

--- src/DiffGenerator.php
+++ src/DiffGenerator.php
@@ -14,7 +14,7 @@ function read(string $filePath): string
         throw new Exception("The file $filePath does not exists.");
     }
 
-    return file_get_contents(realpath($filePath));
+    return file_get_contents($filePath);
 }
 
 function genDiff(string $firstFilePath, string $secondFilePath, string $format = 'stylish'): string
